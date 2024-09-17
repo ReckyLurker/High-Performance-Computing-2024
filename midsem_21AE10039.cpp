92b3b12b96a928cd6ff41f7573ed7e6292933370
Tanish Jain <jtanish120204@gmail.com>
2024-09-17 07:22:16 +0530

    Add CG, MINRES, Steepest Descent

--- midsem_21AE10039.cpp
+++ midsem_21AE10039.cpp
@@ -4,39 +4,66 @@
 #include <cmath>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
+#include <unordered_map>
 
-#define RED "\033[31m" // Change Terminal color to Red 
-#define GREEN "\033[32m" // Change Terminal color to Green 
-#define YELLOW "\033[33m" // Change Terminal color to Yellow 
-#define NC "\033[0m" // Reset terminal color 
+#define RED "\e[0;91m" // Change Terminal color to Red 
+#define GREEN "\e[0;92m" // Change Terminal color to Green 
+#define YELLOW "\e[0;93m" // Change Terminal color to Yellow 
+#define NC "\e[0m" // Reset terminal color 
 
 #define VSMALL 1e-12 // Small term for regularization
-#define BATCH_SIZE 5 // Print after this many iterations
+#define BATCH_SIZE 5 // Print after this many iterations (for iterative methods)
 
 // Function Definitions - to be defined after main // 
 std::vector<std::vector<double>> load(const char* filename, std::size_t& n_rows, std::size_t& n_cols);
+void loadSparseMatrix(const char* filename, std::vector<double>& A_values, std::vector<std::size_t>& A_rows, std::vector<std::size_t>& A_cols, std::size_t& n_rows, std::size_t& n_cols);
 std::vector<double> loadvector(const char* filename);
 void diagonal_preconditioning(std::vector<std::vector<double>>& A, std::vector<double>& B);
+void diagonal_preconditioning(std::vector<double>& A_values, std::vector<double>& B, const std::vector<std::size_t>& A_rows, const std::vector<std::size_t>& A_cols);
 double norm(const std::vector<double>& x, const std::vector<double>& x0); 
 std::vector<double> jacobi(const std::vector<std::vector<double>>& A, const std::vector<double>& B, std::vector<double> x0, const double tol , const int max_iter); 
 std::vector<double> gauss_seidel(const std::vector<std::vector<double>>& A, const std::vector<double>& B, std::vector<double> x0, const double tol , const int max_iter); 
 std::vector<double> gauss_seidel_sor(const std::vector<std::vector<double>>& A, const std::vector<double>& B, const double omega, std::vector<double> x0, const double tol, const int max_iter);
+std::vector<double> cg(const std::vector<std::vector<double>>& A, const std::vector<double>& B, std::vector<double> x0, const double tol, const int max_iter);
+std::vector<double> minres(const std::vector<std::vector<double>>& A, const std::vector<double>& B, std::vector<double> x0, const double tol, const int max_iter);
+std::vector<double> steepest_descent(const std::vector<std::vector<double>>& A, const std::vector<double>& B, std::vector<double> x0, const double tol, const int max_iter);
 
 
 int main() {
 
-    // Loading into the space // 
     std::size_t n_rows = 0, n_cols = 0;
-    std::vector<std::vector<double>> A = load("matrix.txt", n_rows, n_cols); // Load Dense Matrix into space // 
+
+    // Uncomment for loading dense matrix // 
+    std::vector<std::vector<double>> A = load("matrix.txt", n_rows, n_cols); 
+
+    // Uncomment for loading sparse matrix // 
+    
+    // std::vector<double> A_values;
+    // std::vector<std::size_t> A_rows, A_cols; 
+    // loadSparseMatrix("matrix.txt", A_values, A_rows, A_cols, n_rows, n_cols);
+
+    // Load source vector //
     std::vector<double> B = loadvector("sourcevec.txt"); 
     
-    diagonal_preconditioning(A,B);
+    diagonal_preconditioning(A,B); // For dense matrices 
 
-    // Solve Using Different Iterative Methods // 
+    // diagonal_preconditioning(A_values, B, A_rows, A_cols); // For sparse matrices 
+
+    // Solve Dense Linear System Using Different Iterative Methods // 
     std::vector<double> x0(B.size(), 0.0);
+    std::vector<double> x0_temp = x0;
     std::vector<double> x_jacobi = jacobi(A, B, x0, 1e-6, 1000); // jacobi(A, B, initial_guess, tol, max_iter);
+    x0 = x0_temp;
     std::vector<double> x_gs = gauss_seidel(A, B, x0, 1e-6, 1000); // gauss_seidel(A, B, initial_guess, tol, max_iter)
+    x0 = x0_temp;
     std::vector<double> x_gs_sor = gauss_seidel_sor(A, B, 1.5, x0, 1e-6, 1000); // gauss_seidel_sor(A, B, omega, initial_guess, tol, max_iter)
+    x0 = x0_temp;
+    std::vector<double> x_cg = cg(A, B, x0, 1e-6, 1000); // cg(A, B, initial_guess, tol, max_iter)
+    x0 = x0_temp;
+    std::vector<double> x_minres = minres(A, B, x0, 1e-6, 1000); // minres(A, B, initial_guess, tol, max_iter)
+    x0 = x0_temp;
+    std::vector<double> x_steepest_descent = steepest_descent(A, B, x0, 1e-6, 1000); // steepest_descent(A, B, initial_guess, tol, max_iter)
     return 0;
 }
 
@@ -77,6 +104,24 @@ std::vector<std::vector<double>> load(const char* filename, std::size_t& n_rows,
     }
     file.close();
     return data; // Update the Matrix 
+}
+
+// Load Sparse Matrix in CSC format // 
+void loadSparseMatrix(const char* filename, std::vector<double>& A_values, std::vector<std::size_t>& A_rows, std::vector<std::size_t>& A_cols, std::size_t& n_rows, std::size_t& n_cols){
+    std::ifstream file(filename);
+    
+    std::size_t row, col; 
+    double value;
+
+    while(file >> row >> col >> value){
+        A_rows.push_back(row);
+        A_cols.push_back(col);
+        A_values.push_back(value);
+    }
+
+    // Not correct theoretically, but should work for all matrices if complete diagonal provided //
+    n_rows = *std::max_element(A_rows.begin(), A_rows.end());
+    n_cols = *std::max_element(A_cols.begin(), A_cols.end());
 }
 
 // Load Source Vector from ASCII file // 
@@ -123,7 +168,38 @@ void diagonal_preconditioning(std::vector<std::vector<double>>& A, std::vector<d
     }
 }
 
-// Returns the second norm of the vector // 
+// Perform diagonal preconditioning for sparse matrix in CSC format // 
+void diagonal_preconditioning(std::vector<double>& A_values, std::vector<double>& B, const std::vector<std::size_t>& A_rows, const std::vector<std::size_t>& A_cols) {
+    // Find the indices of diagonal elements // 
+    std::unordered_map<std::size_t, std::size_t> diag_ids;  
+    std::vector<double> diagonal; 
+    for(std::size_t i = 0; i < A_rows.size() ; ++i){
+        if(A_rows[i] == A_cols[i]){
+            diag_ids.insert({A_rows[i], i}); // store for future use 
+            diagonal.push_back(A_values[i]);
+        }
+    }
+
+    // Invert Diagonal and scale down source vector B // 
+    std::vector<double> diag_inv(diagonal.size(), 0.0); 
+    for(std::size_t i = 0; i < diagonal.size(); ++i){
+        diag_inv[i] = 1.0/(diagonal[i] + VSMALL);
+    }
+
+    // Scale down A // 
+    for(std::size_t i = 0; i < A_rows.size(); ++i){
+        std::size_t idx = diag_ids[A_rows[i]]; // Index of this diagonal - Not cache friendly
+        A_values[i] *= diag_inv[idx];
+    }
+
+    // Scale down B // 
+    for(std::size_t i = 0; i < A_rows.size(); ++i){
+        std::size_t idx = diag_ids[i]; // Index of the diagonal 
+        B[i] *= diag_inv[idx];
+    }
+}
+
+// Returns the second norm of the vector subtraction x - x0 // 
 double norm(const std::vector<double>& x, const std::vector<double>& x0){
     if(x.size() != x0.size()){
         std::string error = std::string(RED) + std::string("Incompatible Vectors") + std::string(NC) + std::string("\n");
@@ -185,12 +261,12 @@ std::vector<double> jacobi(const std::vector<std::vector<double>>& A, const std:
             std::cout << GREEN << "Solution Converged" << NC << std::endl;
             return x;
         }
+        x0 = x; // Copy the new iteration to the old iteration 
+        ++iter;
         if(iter >= last_iter + BATCH_SIZE){
             last_iter = iter; 
             std::cout << YELLOW << "Jacobi Method -> Residual: " << residual << " Iterations: " << iter << NC << "\n";
         }
-        x0 = x; // Copy the new iteration to the old iteration 
-        ++iter;
     }
     std::cout << RED << "Maximum Iterations reached." << NC << std::endl;
     return x;
@@ -243,12 +319,12 @@ std::vector<double> gauss_seidel(const std::vector<std::vector<double>>& A, cons
             std::cout << GREEN << "Solution Converged" << NC << std::endl;
             return x;
         }
+        x0 = x; // Copy the new iteration to the old iteration 
+        ++iter;
         if(iter >= last_iter + BATCH_SIZE){
             last_iter = iter; 
             std::cout << YELLOW << "Gauss-Seidel Method -> Residual: " << residual << " Iterations: " << iter << NC << "\n";
         }
-        x0 = x; // Copy the new iteration to the old iteration 
-        ++iter;
     }
     std::cout << RED << "Maximum Iterations reached." << NC << std::endl;
     return x;
@@ -264,7 +340,7 @@ std::vector<double> gauss_seidel_sor(const std::vector<std::vector<double>>& A, 
     std::size_t n_cols = A[0].size();
 
     if(n_rows != n_cols){
-        std::string error = std::string(RED) + std::string("Gauss Seidel only for Square Matrices") + std::string(NC) + std::string("\n");
+        std::string error = std::string(RED) + std::string("Gauss Seidel SOR only for Square Matrices") + std::string(NC) + std::string("\n");
         throw std::runtime_error(error);
     }
 
@@ -298,17 +374,263 @@ std::vector<double> gauss_seidel_sor(const std::vector<std::vector<double>>& A, 
         double residual = norm(x, x0);
         if(residual < tol){
             x0 = x; // Copy the new iteration to the old iteration
-            std::cout << GREEN << "Gauss-Seidel Method -> Final Residual: " << residual << " Iterations: " << iter << NC << "\n";
+            std::cout << GREEN << "Gauss-Seidel SOR Method -> Final Residual: " << residual << " Iterations: " << iter << NC << "\n";
             std::cout << GREEN << "Solution Converged" << NC << std::endl;
             return x;
         }
-        if(iter >= last_iter + BATCH_SIZE){
-            last_iter = iter; 
-            std::cout << YELLOW << "Gauss-Seidel Method -> Residual: " << residual << " Iterations: " << iter << NC << "\n";
-        }
         x0 = x; // Copy the new iteration to the old iteration 
         ++iter;
+        if(iter >= last_iter + BATCH_SIZE){
+            last_iter = iter; 
+            std::cout << YELLOW << "Gauss-Seidel SOR Method -> Residual: " << residual << " Iterations: " << iter << NC << "\n";
+        }
     }
     std::cout << RED << "Maximum Iterations reached." << NC << std::endl;
+    return x;
+}
+
+// Krylov SUBSPACE METHODS // 
+
+// Matrix-Vector Multiplication // 
+std::vector<double> matvecmul(const std::vector<std::vector<double>>& A, const std::vector<double> B){
+    std::size_t n_rows = A.size();
+    if(n_rows == 0){
+        std::string error = std::string(RED) + std::string("Empty Matrix A") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    } 
+    std::size_t n_cols = A[0].size();
+    if(n_cols != B.size()){
+        std::string error = std::string(RED) + std::string("Incompatible Matrix Vector Product") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    }
+    std::vector<double> res(n_rows, 0.0);
+    for(std::size_t i = 0; i < n_rows; ++i){
+        for(std::size_t j = 0 ; j < n_cols ; ++j){
+            res[i] += A[i][j] * B[j]; // Matrix vector product
+        }
+    }
+    return res; 
+}
+
+// Dot Product (Vector) // 
+double dot(const std::vector<double>& x, const std::vector<double>& y){
+    if(x.size() != y.size()){
+        std::string error = std::string(RED) + std::string("Incompatible vectors for dot Product") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    }
+    double sum = 0.0;
+    for(std::size_t i = 0; i < x.size(); ++i){
+        sum += x[i]*y[i];
+    }
+    return sum;
+}
+
+// Dot Product (Matrix Vector) // 
+std::vector<double> dot(const std::vector<std::vector<double>>& A, const std::vector<double>& B){
+    std::size_t n_rows = A.size();
+    if(n_rows == 0){
+        std::string error = std::string(RED) + std::string("Empty Matrix A") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    }
+    if(n_rows != B.size()){
+        std::string error = std::string(RED) + std::string("Incompatible matrix-vector for dot Product") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    }
+    std::size_t n_cols = A[0].size();
+
+    std::vector<double> res(n_cols, 0.0);
+    for(std::size_t i = 0; i < n_rows; ++i){
+        for(std::size_t j = 0; j < n_cols ; ++j){
+            res[j] += A[i][j] * B[j]; // Cache Friendly access
+        }
+    }
+    return res;
+}
+
+// Add (vector + vector) // 
+std::vector<double> add(const std::vector<double>& x, const std::vector<double>& y){
+    if(x.size() != y.size()){
+        std::string error = std::string(RED) + std::string("Incompatible vectors for addition") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    }
+    std::vector<double> res(x.size());
+    for(std::size_t i = 0; i < x.size(); ++i){
+        res[i] = x[i] + y[i];
+    }
+    return res;
+}
+
+// Subtract (vector - vector) // 
+std::vector<double> subtract(const std::vector<double>& x, const std::vector<double>& y){
+    if(x.size() != y.size()){
+        std::string error = std::string(RED) + std::string("Incompatible vectors for subtraction") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    }
+    std::vector<double> res(x.size());
+    for(std::size_t i = 0; i < x.size(); ++i){
+        res[i] = x[i] - y[i];
+    }
+    return res;
+}
+
+// Scale a vector by a scalar // 
+std::vector<double> scale(const std::vector<double>& x, const double& alpha){
+    std::vector<double> res(x.size());
+    for(std::size_t i = 0; i < x.size(); ++i){
+        res[i] = x[i] * alpha;
+    }
+    return res;
+}
+
+// Conjugate Gradient Iterative Method // 
+std::vector<double> cg(const std::vector<std::vector<double>>& A, const std::vector<double>& B, std::vector<double> x0 = std::vector<double>(), const double tol = 1e-6, const int max_iter = 1000) {
+    std::size_t n_rows = A.size();
+    if(n_rows == 0){
+        std::string error = std::string(RED) + std::string("Empty Matrix A") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    } 
+    std::size_t n_cols = A[0].size();
+
+    if(n_rows != n_cols){
+        std::string error = std::string(RED) + std::string("Conjugate Gradient only for Square Matrices") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    }
+
+    if(x0.size() == 0){
+        x0 = std::vector<double>(n_rows, 0.0);
+    }
+
+    std::vector<double> x = x0; 
+    std::vector<double> res = subtract(B, matvecmul(A,x));
+    std::vector<double> p = res; 
+    std::vector<double> Ap = matvecmul(A,p);
+
+    int iter = 0; 
+    int last_iter = 0; 
+
+    double rs_old = dot(res,res);
+    double residual = 1e6;
+
+    while(residual > tol && iter <= max_iter){
+        Ap = matvecmul(A, p);
+        rs_old = dot(res,res);
+        double alpha = rs_old/(dot(Ap,p) + VSMALL);
+        x = add(x, scale(p, alpha));
+        res = subtract(res, scale(Ap, alpha));
+        residual = norm(x,x0);
+        x0 = x;
+        if(residual < tol){
+            std::cout << GREEN << "Conjugate Gradient Method -> Final Residual: " << residual << " Iterations: " << iter << NC << "\n";
+            std::cout << GREEN << "Solution Converged" << NC << std::endl;
+            return x;
+        }
+        p = add(res, scale(p, dot(res,res) / (rs_old + VSMALL)));
+        ++iter;
+        if(iter >= last_iter + BATCH_SIZE){
+            last_iter = iter; 
+            std::cout << YELLOW << "Conjugate Gradient Method -> Residual: " << residual << " Iterations: " << iter << NC << "\n";
+        } 
+    }
+    std::cout << RED << "Maximum Iterations reached or solution couldn't converge." << NC << std::endl;
+    return x;
+}
+
+// Minimum Residual Iterations (MINRES) // 
+std::vector<double> minres(const std::vector<std::vector<double>>& A, const std::vector<double>& B, std::vector<double> x0 = std::vector<double>(), const double tol = 1e-6, const int max_iter = 1000) {
+    std::size_t n_rows = A.size();
+    if(n_rows == 0){
+        std::string error = std::string(RED) + std::string("Empty Matrix A") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    } 
+    std::size_t n_cols = A[0].size();
+
+    if(n_rows != n_cols){
+        std::string error = std::string(RED) + std::string("Minimum Residual only for Square Matrices") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    }
+
+    if(x0.size() == 0){
+        x0 = std::vector<double>(n_rows, 0.0);
+    }
+
+    std::vector<double> x = x0; 
+    std::vector<double> res = subtract(B, matvecmul(A,x));
+    std::vector<double> p = matvecmul(A,res);
+
+    int iter = 0; 
+    int last_iter = 0;
+    double residual = 1e6; 
+
+    while(residual > tol && iter <= max_iter){
+        double alpha = dot(p,res) / (dot(p,p) + VSMALL);
+        x = add(x, scale(res, alpha));
+        residual = norm(x,x0);
+        x0 = x;
+        res = subtract(res, scale(p, alpha));
+        p = matvecmul(A, res);
+
+        if(residual < tol){
+            std::cout << GREEN << "Minimum Residual Method -> Final Residual: " << residual << " Iterations: " << iter << NC << "\n";
+            std::cout << GREEN << "Solution Converged" << NC << std::endl;
+            return x;
+        }
+        ++iter;
+        if(iter >= last_iter + BATCH_SIZE){
+            last_iter = iter; 
+            std::cout << YELLOW << "Minimum Residual Method -> Residual: " << residual << " Iterations: " << iter << NC << "\n";
+        } 
+    }
+    std::cout << RED << "Maximum Iterations reached or solution couldn't converge." << NC << std::endl;
+    return x;
+}
+
+// Steepest Descent Iteration // 
+std::vector<double> steepest_descent(const std::vector<std::vector<double>>& A, const std::vector<double>& B, std::vector<double> x0 = std::vector<double>(), const double tol = 1e-6, const int max_iter = 1000){
+    std::size_t n_rows = A.size();
+    if(n_rows == 0){
+        std::string error = std::string(RED) + std::string("Empty Matrix A") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    } 
+    std::size_t n_cols = A[0].size();
+
+    if(n_rows != n_cols){
+        std::string error = std::string(RED) + std::string("Steepest Descent only for Square Matrices") + std::string(NC) + std::string("\n");
+        throw std::runtime_error(error);
+    }
+
+    if(x0.size() == 0){
+        x0 = std::vector<double>(n_rows, 0.0);
+    }
+
+    std::vector<double> x = x0; 
+    std::vector<double> res = subtract(B, matvecmul(A,x));
+    std::vector<double> v = dot(A,res); // equivalent to A.t * res
+    std::vector<double> Av = matvecmul(A,v);
+
+    int iter = 0; 
+    int last_iter = 0;
+    double residual = 1e6; 
+
+    while(residual > tol && iter <= max_iter){
+        v = dot(A,res);
+        Av = matvecmul(A,v);
+        double alpha = dot(v,v)/(dot(Av,Av) + VSMALL);
+        x = add(x, scale(v, alpha));
+        residual = norm(x,x0);
+        x0 = x;
+        res = subtract(res, scale(Av, alpha));
+
+        if(residual < tol){
+            std::cout << GREEN << "Steepest Descent Method -> Final Residual: " << residual << " Iterations: " << iter << NC << "\n";
+            std::cout << GREEN << "Solution Converged" << NC << std::endl;
+            return x;
+        }
+        ++iter;
+        if(iter >= last_iter + BATCH_SIZE){
+            last_iter = iter; 
+            std::cout << YELLOW << "Steepest Descent Method -> Residual: " << residual << " Iterations: " << iter << NC << "\n";
+        } 
+    }
+    std::cout << RED << "Maximum Iterations reached or solution couldn't converge." << NC << std::endl;
     return x;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,140 +1,20 @@
-/* 
-    Assignment-2 
-    Author: Tanish Jain (recklurker)
-    Roll Number: 21AE10039
-    Generates Finite Difference Matrices for the Laplacian Problem
-*/
-
 #include <iostream> 
-#include <armadillo> 
+#include <armadillo>
 
-#define YELLOW "\033[33m" // Change Terminal color to Yellow 
-#define NC "\033[0m" // Reset terminal color 
-
-void generateDenseMatrix(size_t nx, size_t ny, double dx, double dy){
-    size_t mat_size = (nx+1)*(ny+1); 
-    double dx_inv = 1.0/dx;
-    double dy_inv = 1.0/dy; 
-    arma::Mat<double> A = arma::zeros<arma::Mat<double>>(mat_size, mat_size);
-
-    // Boundary Condition - Bottom (y = 0)
-    for(size_t i = 0; i<nx+1; ++i){
-        A(i*(ny+1), i*(ny+1)) = 1.0; // j = 0 
-    }
-
-    // Boundary Condition - Left (x = 0)
-    for(size_t j = 0; j<ny+1; ++j){
-        A(j, j) = 1.0; // i = 0
-    }
-
-    // Boundary Condition - Right (x = Lx)
-    for(size_t j = 0; j<ny+1; ++j){
-        A(nx*(ny+1) + j, nx*(ny+1) + j) = 1.0; // i = nx 
-    }
-
-    // Boundary Condition - Top (y = Ly)
-    for(size_t i = 0; i < nx+1 ; ++i){
-        A(i*(ny+1) + ny, i*(ny+1) + ny) = 1.0; // j = ny 
-    }
-
-    // For the rest of the elements - 5 Point Stencil // 
-    for(size_t i = 1; i<nx; ++i){
-        for(size_t j = 1; j < ny; ++j){
-            size_t k = i*(ny+1) + j; 
-            A(k, k) = 2*dx_inv*dx_inv + 2*dy_inv*dy_inv; 
-            A(k, k+1) = -dy_inv*dy_inv; 
-            A(k, k-1) = -dy_inv*dy_inv; 
-            A(k, k + ny+1) = -dx_inv*dx_inv; 
-            A(k, k - (ny+1)) = -dx_inv*dx_inv; 
-        }
-    }
-    A.save("../matrix.txt", arma::raw_ascii);
-}
-
-void generateSparseMatrix(size_t nx, size_t ny, double dx, double dy){
-    size_t mat_size = (nx+1)*(ny+1);
-    double dx_inv = 1.0/dx;
-    double dy_inv = 1.0/dy; 
-
-    arma::SpMat<double> A(mat_size, mat_size);
-
-    // Boundary Condition - Bottom (y = 0)
-    for(size_t i = 0; i<nx+1; ++i){
-        A(i*(ny+1), i*(ny+1)) = 1.0; // j = 0 
-    }
-
-    // Boundary Condition - Left (x = 0)
-    for(size_t j = 0; j<ny+1; ++j){
-        A(j, j) = 1.0; // i = 0
-    }
-
-    // Boundary Condition - Right (x = Lx)
-    for(size_t j = 0; j<ny+1; ++j){
-        A(nx*(ny+1) + j, nx*(ny+1) + j) = 1.0; // i = nx 
-    }
-
-    // Boundary Condition - Top (y = Ly)
-    for(size_t i = 0; i < nx+1 ; ++i){
-        A(i*(ny+1) + ny, i*(ny+1) + ny) = 1.0; // j = ny 
-    }
-
-    // For the rest of the elements - 5 Point Stencil // 
-    for(size_t i = 1; i<nx; ++i){
-        for(size_t j = 1; j < ny; ++j){
-            size_t k = i*(ny+1) + j; 
-            A(k, k) = 2*dx_inv*dx_inv + 2*dy_inv*dy_inv; 
-            A(k, k+1) = -dy_inv*dy_inv; 
-            A(k, k-1) = -dy_inv*dy_inv; 
-            A(k, k + ny+1) = -dx_inv*dx_inv; 
-            A(k, k - (ny+1)) = -dx_inv*dx_inv; 
-        }
-    }
-    A.save("../matrix.txt", arma::coord_ascii);
-}
-
-void generateSourceVector(size_t nx, size_t ny){
-    size_t vec_size = (nx+1)*(ny+1);
-
-    arma::Col<double> sourceVector = arma::zeros<arma::Col<double>>(vec_size);
-
-    for(size_t i = 0; i < nx+1 ; ++i){
-        sourceVector(i*(ny+1) + ny) = 1.0; // T = 1 on top  
-    }
-
-    sourceVector.save("../sourcevec.txt", arma::raw_ascii);
-}
-
+#include "matrix.hpp" // Batchwise loading of sparse matrices
+#include "diagonal_preconditioning.hpp" // Diagonal Preconditioning of sparse and dense matrices 
+#include "jacobi.hpp" // Jacobi iterative method for sparse and dense matrices 
 
 int main(void){
-    // Basic IO 
 
-    double dx, dy; // Step Sizes 
-    double Lx, Ly; // Grid Sizes
-    size_t nx, ny; // Number of points in Grid 
+    arma::SpMat<double> A = loadCoordAscii("../matrix.txt");  // If given in sparse format (CSC)
+    // arma::Mat<double> A; // If given in dense format
+    // A.load("../matrix.txt");
+    // Load source vector // 
+    arma::Col<double> B;
+    B.load("../sourcevec.txt", arma::raw_ascii); // Source Vector will always be in raw ascii form // 
+    
+    diagonal_preconditioning(A, B); // Diagonal (Jacobi) Preconditioning to reduce condition number    
 
-    std::cout << "Enter dx: "; 
-    std::cin >> dx;
-    std::cout << "Enter dy: ";
-    std::cin >> dy; 
-    std::cout << "Enter Lx: ";
-    std::cin >> Lx;
-    std::cout << "Enter Ly: ";
-    std::cin >> Ly; 
-
-    nx = (size_t)(Lx/dx);
-    ny = (size_t)(Ly/dy);
-
-    std::cout << YELLOW << "Number of Elements in X: " << nx << NC << "\n";
-    std::cout << YELLOW << "Number of Elements in Y: " << ny << NC << "\n";
-
-    if(nx*ny <= 100){
-        generateDenseMatrix(nx, ny, dx, dy); // Generate A in dense format.
-    }
-    else{
-        generateSparseMatrix(nx, ny, dx, dy); // Generate A in sparse format, avoid overheads.
-    }   
-
-    generateSourceVector(nx, ny); // Generate B 
-
-    return 0; 
+    arma::Col<double> x_jacobi = jacobi(A, B, arma::zeros(A.n_rows), 1e-15, 50000); // Jacobi Method
 }
